4b583f5e2a3cb7c0980d61023acc18af51d89f0a
Idrwla <ilyarredmi@gmail.com>
2020-10-10 17:48:43 +0600

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+  title = 'Angular APP';
+  greet = 'grett';
+
+  getvalue = (num: number) => {
+    let a: any = document.forms[0]["textview"];
+    a.value += num;
+  }
+
+  getopcode = (opt: string) => {
+    let a: any = document.forms[0]["textview"];
+
+    if (a.value !== "") {
+      if (opt === "s") {
+        a.value += "**2";
+      } else {
+        a.value += opt;
+      }
+    } else if (opt === "(" || opt === ")") {
+      a.value += opt;
+    } else if (opt === '.') {
+      a.value += "0.";
+
+    }
+
+  }
+
+  zero = () => {
+    let a: any = document.forms[0]["textview"];
+    if (a.value !== "") {
+      a.value += "0";
+    }
+  }
+  clean = () => {
+    let a: any = document.forms[0]["textview"];
+    a.value = '';
+    let b: any = document.forms[0]["result"];
+    b.value = '';
+  }
+  equal = () => {
+    let a: any = document.forms[0]["result"];
+    let temp: any = eval(document.forms[0]["textview"].value);
+
+    if (temp === "Infinity") {
+      a.value = "Hа ноль нельзя делить";
+    } else if (document.forms[0]["textview"].value === '') {
+      a.value = "Введите выражение";
+
+    } else {
+
+      a.value = temp;
+      let table: any = document.getElementsByTagName("table")[0];
+      if (table.rows.length >= 3) {
+        table.deleteRow(0);
+      }
+      let trow: any = table.insertRow(table.rows.length);
+      let cel0: any = trow.insertCell(0);
+      let cel1: any = trow.insertCell(1);
+      let cel2: any = trow.insertCell(2);
+      cel0.innerHTML = document.forms[0]["textview"].value;
+      cel1.innerHTML = "=";
+      cel2.innerHTML = temp;
+      this.clean();
+    }
+
+  }
+  back = () => {
+    let a: any = document.forms[0]["textview"];
+    a.value = document.forms[0]["textview"].value.substring(0, document.forms[0]["textview"].value.length - 1);
+    let b: any = document.forms[0]['result'];
+    b.value = '';
+  }
 }
